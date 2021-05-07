5f1699ed89bc37c22990b9216394f85171c091dc
sk <sk.paradoxmaske@gmail.com>
2021-05-07 11:35:58 -0500

    get rid of preloader

--- src/App.js
+++ src/App.js
@@ -8,47 +8,14 @@ import Skills from './components/Skills';
 import { GlobalStyle } from './App.style';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import PuffLoader from "react-spinners/PuffLoader";
-import { css } from "@emotion/core";
-
-const override = css`
-  display: block;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  /* first border */
-  .css-shq65p{
-    border: solid #ff1842 thick;
-  }
-
-  /* second border */
-  .css-8r04rc{
-    border: solid #3AF8D6 thick;
-  }
-`
 
 function App() {
   //!
   // TODOs: custom mouse, mobile responsive, button animation, nav animation, about section animation, contact animation, link resume
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() =>{
-    setIsLoading(false);
-    AOS.init({duration: 700});
-  }, [])
 
   return (
-    <>
-    <GlobalStyle />
-    {
-      isLoading ? 
-      <>
-        <PuffLoader loading={isLoading} size={200} css={override}/>
-        <p style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%,-50%)"}}>Loading...</p>
-      </>:
       <div className="App">
+        <GlobalStyle />
         <Nav />
         <Intro />
         <About />
@@ -56,8 +23,6 @@ function App() {
         <Projects />
         <Contacts />
       </div>
-    }
-    </>
   );
 }
 
